98f7881b3de0042f961dfd1b7560e7b2cacd659a
sacid <samedsacid.soyalan@tomdigital.com.tr>
2025-03-24 03:53:38 +0100

    [DEV][sacid.soyalan][yml düzenlendi]

--- app/src/main/java/com/sasasoyalan/composecomponents/componentpreviews/CollapsingHeaderViewPreview.kt
+++ app/src/main/java/com/sasasoyalan/composecomponents/componentpreviews/CollapsingHeaderViewPreview.kt
@@ -6,9 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
